34639fa7ed3fedd482bdab8fbdfe8c5768fec518
Ken Messmer <kenmessmerjr@gmail.com>
2020-12-25 01:52:54 -0600

    Further work with variables

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-character_name = "Tom"
-character_age = "50"
+character_name = "John McClane"
+character_age = "40"
 print("There once was a man named " + character_name + ", ")
 print("he was " + character_age + " years old. ")
 print("He really liked the name " + character_name + ", ")
